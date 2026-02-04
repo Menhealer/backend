7f7f94a20074cdf104123f10fccca92ac2a9d3a9
alpin87 <qortmdals94@naver.com>
2026-02-04 17:34:56 +0900

    fix: Objext Storage config change

--- src/main/java/com/relog/relog/config/OciObjectStorageConfig.java
+++ src/main/java/com/relog/relog/config/OciObjectStorageConfig.java
@@ -1,8 +1,6 @@
 package com.relog.relog.config;
 
 import com.oracle.bmc.Region;
-import com.oracle.bmc.auth.BasicAuthenticationDetailsProvider;
-import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
 import com.oracle.bmc.auth.InstancePrincipalsAuthenticationDetailsProvider;
 import com.oracle.bmc.objectstorage.ObjectStorageClient;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,21 +10,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OciObjectStorageConfig {
 
-    @Value("${oci.auth.type:instance-principal}")
-    private String authType;
-
     @Value("${oci.region:ap-chuncheon-1}")
     private String region;
 
     @Bean
-    public ObjectStorageClient objectStorageClient() throws Exception {
-        BasicAuthenticationDetailsProvider provider;
-
-        if ("config-file".equals(authType)) {
-            provider = new ConfigFileAuthenticationDetailsProvider("~/.oci/config", "DEFAULT");
-        } else {
-            provider = InstancePrincipalsAuthenticationDetailsProvider.builder().build();
-        }
+    public ObjectStorageClient objectStorageClient() {
+        InstancePrincipalsAuthenticationDetailsProvider provider =
+                InstancePrincipalsAuthenticationDetailsProvider.builder().build();
 
         return ObjectStorageClient.builder()
                 .region(Region.fromRegionId(region))
